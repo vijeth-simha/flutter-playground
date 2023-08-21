ea3b1340efa6a741c14601014a920c10c2eebd53
vijeth_simha <vijethsimha71@gmail.com>
2023-08-22 00:30:31 +0530

    hot reload done

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,30 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Hello "),
-          centerTitle: true,
-        ),
-        body: const Center(
-          child: Text(
-            "Hey Vijeth",
-            style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.amber),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: const Text("Click"),
-          onPressed: () {
-            debugPrint('Hello Vijeth sdsdi!');
-          },
+void main() => runApp(const MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Appbar"),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text(
+          "Hey Stateless widget",
+          style: TextStyle(
+              fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.amber),
         ),
       ),
-    ));
+      floatingActionButton: FloatingActionButton(
+        child: const Text("+"),
+        onPressed: () {
+          debugPrint('Hello Vijeth!!!');
+        },
+      ),
+    );
+  }
+}

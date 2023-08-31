d445f3a1cd456ecb5aa9b6cdc72ef8ad5d745f48
vijeth_simha <vijethsimha71@gmail.com>
2023-08-31 23:26:01 +0530

    added spinner package

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_playground/services/api_service.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -27,10 +28,12 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(50.0),
-        child: Text('Loading...'),
-      ),
-    );
+        backgroundColor: Colors.blue,
+        body: Center(
+          child: SpinKitRotatingCircle(
+            color: Colors.white,
+            size: 50.0,
+          ),
+        ));
   }
 }

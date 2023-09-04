0cc31ec4a68867a27c343a27393c0b87a809c721
vijeth_simha <vijethsimha71@gmail.com>
2023-09-04 23:21:44 +0530

    pushed the code

--- lib/pages/forms.dart
+++ lib/pages/forms.dart
@@ -14,6 +14,7 @@ class _FormsState extends State<Forms> {
     return Scaffold(
       appBar: AppBar(title: const Text("Form")),
       body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 0.0),
         child: Form(
             key: formKey,
             child: Column(
@@ -22,7 +23,34 @@ class _FormsState extends State<Forms> {
                 TextFormField(
                   decoration:
                       const InputDecoration(labelText: "Enter your name"),
-                )
+                  validator: (value) {
+                    if (value!.isNotEmpty ||
+                        !RegExp(r'/^[A-Za-z]+$/').hasMatch(value!)) {
+                      return "Enter the name correctly";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                TextFormField(
+                  decoration:
+                      const InputDecoration(labelText: "Enter your email"),
+                  validator: (value) {
+                    if (value!.isNotEmpty ||
+                        !RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
+                            .hasMatch(value)) {
+                      return "Enter the email correctly";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      print(formKey.currentState);
+                      if (formKey.currentState!.validate()) {}
+                    },
+                    child: const Text("Submit"))
               ],
             )),
       ),

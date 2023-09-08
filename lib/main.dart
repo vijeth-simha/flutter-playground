b3bab3c86f633182a4100cd55d1e49eda8cd0d89
vijeth_simha <vijethsimha71@gmail.com>
2023-09-08 23:57:42 +0530

    added the riverpod code display

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final helloWorldProvider = Provider((_) => 'Hello world');
+final helloWorldProvider = Provider((ref) => "Vijeth Simha");
 void main() {
   runApp(
     // For widgets to be able to read providers, we need to wrap the entire
     // application in a "ProviderScope" widget.
     // This is where the state of our providers will be stored.
-    ProviderScope(
+    const ProviderScope(
       child: MyApp(),
     ),
   );
